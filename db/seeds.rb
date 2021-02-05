808e16f13b2edab4770121e9d73981ba82ae10c1
david-forer <david.j.forer@gmail.com>
2021-02-05 16:51:36 +0900

    added users and seeded 20

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+(1..20).each do |id| 
+    User.create!(
+        # each user is assigned an id from 1-20
+        id: id, 
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+# issue each user the same password
+        password: "password", 
+        password_confirmation: "password",
+    )
+end
